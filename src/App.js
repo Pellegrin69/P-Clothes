5016095a18e4e146970339ff6559b18476f0ecb4
pellegrin.pierre <pierre.pellegrin@esqese.ucly.fr>
2021-04-04 20:21:45 +0200

    Hello World test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hélau Waur   d</h1>
+      <h1>Hélio Whore  d</h1>
     </div>
   );
 }
